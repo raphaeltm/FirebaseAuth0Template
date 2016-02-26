cd08c57d629cd2e690f7e651fba3b7a0a2011aa5
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2016-02-26 10:26:40 -0800

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name auth0FirebaseTemplateApp
+ * @description
+ * # auth0FirebaseTemplateApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('auth0FirebaseTemplateApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name auth0FirebaseTemplateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the auth0FirebaseTemplateApp
+ */
+angular.module('auth0FirebaseTemplateApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
